5cd77e7873383356b9b1d713011edf8dcebafd53
Shreyash Kale <shreyashkale9@gmail.com>
2022-12-08 19:52:57 +0530

    First

---
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -0,0 +1,35 @@
+import useFetch from "../../hooks/useFetch";
+import "./featuredProperties.css";
+
+const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+
+  return (
+    <div className="fp">
+      {loading ? (
+        "Loading"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img
+                  src="https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                alt=""
+                className="fpImg"
+              />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+              {item.rating && <div className="fpRating">
+                <button>{item.rating}</button>
+                <span>Excellent</span>
+              </div>}
+            </div>
+          ))}
+        </>
+      )}
+    </div>
+  );
+};
+
+export default FeaturedProperties;
